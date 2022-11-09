22a3522f355f74398208f4b75722318da216a47c
dalian tillier <daliandu62@hotmail.fr>
2022-11-09 14:14:06 +0100

    Commit 4 : Ajout Client

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,9 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Home from './pages/Home';
 import Test from './pages/Test';
 import PageClient from './pages/PageClient';
-import SuppClient from './pages/SuppClient';
-import MajClient from './pages/MajClient';
+import SuppClient from "./pages/SuppClient";
+import MajClient from "./pages/MajClient";
+import PageAjout from "./pages/PageAjout";
 
 function App() {
   return (
@@ -13,9 +14,10 @@ function App() {
         <Route path="/" element={<Home />} /> //charger le composant Home
         <Route path="/test" element={<Test />} /> // quand tu trouve le composant test, le charger
         <Route path="/clients/:id" name="PageClient" element={<PageClient />} /> // route pour le router
+        <Route path="*" element={<Home />} /> // mettre composant * Ã  la fin
         <Route path="/clients/supp/:id" element={<SuppClient />} />
         <Route path="/clients/modif/:id" element={<MajClient />} />
-        <Route path="*" element={<Home />} /> // mettre composant * à la fin
+        <Route path="/clients/ajout" element={<PageAjout />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ front/src/pages/PageAjout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import AjoutClient from "../components/AjoutClient";
+
+const PageAjout = () => {
+  return (
+    <div>
+      <Header />
+      <AjoutClient />
+      <Footer />
+    </div>
+  );
+};
+
+export default PageAjout;
